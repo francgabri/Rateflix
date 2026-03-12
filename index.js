f72661a25c2a4844bb17fcc0ba1dcd372cd0fc2c
franco <francgabri12@gmail.com>
2026-03-12 16:20:34 -0300

    implementacion js

---
+++ index.js
@@ -0,0 +1,11 @@
+let peliculas = []
+
+/* ideas que tengo par este projecto, 
+--n1 que al pasar el cursor por una peli y
+ darle click nos muestre una pantalla a la cual sea solo de esa peli y eso hacerlo para casda peli, esto lo podemos hacer usando css 
+ y html
+ 
+ --n2que la lista de pelioculas este vacia y al dare en un icono sobre la peli (+) se agrege a mi lista 
+ 
+ --n3 que el buscador nos tire recomendaciones antesbuscadas o coincidencias de las peliculas y al darle click nos lleve al apartado del
+ punto n1*/
